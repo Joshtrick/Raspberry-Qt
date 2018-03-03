000cba40483ea253c053ecaa178820b553921a63
Hao Jiang <joshtrick@outlook.com>
2018-03-03 18:17:34 +0800

    Update mythreads.cpp

--- src/mythreads.cpp
+++ src/mythreads.cpp
@@ -2,28 +2,26 @@
 
 void *thread_1(void *ptr)
 {
-  ptr = (void*)(MainWindow*)malloc(sizeof(MainWindow));
+  MainWindow *window = (MainWindow*) ptr;
   while(1)
   {
-    ((MainWindow*)ptr)->mutex_dummy.lock();
-    ((MainWindow*)ptr)->dummy_val = 'A';
-    cout << "t1 -> The dummy value is: " << ((MainWindow*)ptr)->dummy_val << endl;
-    ((MainWindow*)ptr)->mutex_dummy.unlock();
+    window->mutex_dummy.lock();
+    window->dummy_val = 'A';
+    cout << "t1 -> The dummy value is: " << window->dummy_val << endl;
+    window->mutex_dummy.unlock();
     hello_1();
   }
-  free(ptr);
 }
 
 void *thread_2(void *ptr)
 {
-  ptr = (void*)(MainWindow*)malloc(sizeof(MainWindow));
+  MainWindow *window = (MainWindow*) ptr;
   while(1)
   {
-    ((MainWindow*)ptr)->mutex_dummy.lock();
-    ((MainWindow*)ptr)->dummy_val = 'B';
-    cout << "t2 -> The dummy value is: " << ((MainWindow*)ptr)->dummy_val << endl;
-    ((MainWindow*)ptr)->mutex_dummy.unlock();
+    window->mutex_dummy.lock();
+    window->dummy_val = 'B';
+    cout << "t2 -> The dummy value is: " << window->dummy_val << endl;
+    window->mutex_dummy.unlock();
     hello_2();
   }
-  free(ptr);
 }
